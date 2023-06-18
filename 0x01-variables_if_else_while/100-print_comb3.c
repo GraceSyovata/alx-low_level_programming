77559e2d9cb618d60de517d6e62f60e38a56bcd6
GraceSyovata <gracesyovata@yahoo.com>
2023-06-18 02:37:29 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints all possible different combinations of two digits
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+		int i, k = '0', j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = k; j <= '9'; j++)
+		{
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+			}
+			if (i == j)
+			{
+				continue;
+			}
+			if (i == '8' && j == '9')
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		k++;
+	}
+	putchar('\n');
+	return (0);
+}
