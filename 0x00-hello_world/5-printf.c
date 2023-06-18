369349c6ac60bb56c64f719086bfa4c319956548
GraceSyovata <gracesyovata@yahoo.com>
2023-06-18 01:26:28 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+/**
+ * main - A program that prints a line usin printf function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
