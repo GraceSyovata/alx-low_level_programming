26fe1b511792d318b1f36e9803f392c4165e3f2e
GraceSyovata <gracesyovata@yahoo.com>
2023-07-06 11:53:56 -0700

    add

--- 0x08-recursion/100-is_palidrome.c
+++ 0x08-recursion/100-is_palidrome.c
@@ -1,29 +1,24 @@
-#include <stdio.h>
-#include <string.h>
-
+#include "main.h"
 int is_palindrome(char *s) {
     int left = 0;
     int right = strlen(s) - 1;
 
     while (left < right) {
         if (s[left] != s[right]) {
-            return 0;  // Not a palindrome
-        }
+            return 0;
+ 	}
         left++;
         right--;
     }
 
-    return 1; 
+    return 1;  
 }
 
 int main() {
     char str1[] = "radar";
-    printf("%d\n", is_palindrome(str1));
-
+    printf("%d\n", is_palindrome(str1));  
     char str2[] = "hello";
-    printf("%d\n", is_palindrome(str2)); 
-
+    printf("%d\n", is_palindrome(str2));  
     char str3[] = "";
-    printf("%d\n", is_palindrome(str3)); 
-
-    return 0;
+    printf("%d\n", is_palindrome(str3));   return 0;
+}
