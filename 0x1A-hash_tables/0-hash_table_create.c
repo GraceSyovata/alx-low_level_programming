c26c1a2d8cf868043841aa5ce14e02e1501af311
GraceSyovata <gracesyovata@yahoo.com>
2024-02-16 04:53:47 -0800

    readme file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,25 +1,31 @@
 #include "hash_tables.h"
 /**
-  * hash_table_create - creates a hash table
-  * @size: size of the array of the hash table
-  *
-  * Return: Pointer to the newly created hash table or Null if something goes wrong
-  */
+ * hash_table_create - function that creates a hash table.
+ * @size: size of the table
+ * Return: new_table created
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
+
+	hash_table_t *new_table;
 
 	if (size == 0)
 		return (NULL);
 
-	ht = malloc(sizeof(hash_table_t));
-	if (!ht)
+    /* new table space */
+	new_table = malloc(sizeof(hash_table_t));
+
+	if (new_table == NULL)
 		return (NULL);
 
-	ht->size = size;
-	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
-	if (ht->array == NULL)
+	new_table->size = size;
+
+    /* void *calloc(size_t, size_t) */
+	new_table->array = calloc(size, sizeof(hash_node_t *));
+
+	if (new_table->array == NULL)
 		return (NULL);
 
-	return (ht);
+	return (new_table);
 }
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,17 @@
+0x1A. C - Hash tables
+C
+Algorithm
+Data structure
+ By: Julien Barbier
+ Weight: 1
+ Project will start Feb 15, 2024 6:00 AM, must end by Feb 17, 2024 6:00 AM
+ Checker was released at Feb 15, 2024 6:00 PM
+ An auto review will be launched at the deadline
+Resources
+Read or watch:
+
+What is a HashTable Data Structure - Introduction to Hash Tables , Part 0
+Hash function
+Hash table
+All about hash tables
+why hash tables and not arrays
